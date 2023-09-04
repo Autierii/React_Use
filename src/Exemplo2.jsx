b40afca68f776120be33c5695a6638cf6e69c644
rafael <rafael_autieri@hotmail.com>
2023-09-04 11:45:24 -0300

    usando react

---
+++ src/Exemplo2.jsx
@@ -0,0 +1,10 @@
+import {} from 'react'
+
+function Exemplo2(){
+    return(
+        <>
+        <h2>Exemplo2</h2>
+        </>
+    )
+}
+export default Exemplo2
---
+++ src/Exemplo3.jsx
@@ -0,0 +1,10 @@
+import {} from 'react'
+
+function Exemplo3(){
+    return(
+        <>
+        <h3>Exemplo3</h3>
+        </>
+    )
+}
+export default Exemplo3
